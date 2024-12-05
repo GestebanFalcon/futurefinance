ed14c864971800f6153d01a6aa14d8fdace6b83b
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-12-05 10:53:10 -0500

    deleted junk

--- src/actions/editUser.ts
+++
@@ -1,9 +0,0 @@
-"use server"
-
-import { SelectBankAccount } from "@/lib/drizzy/schema/other";
-
-export default function editBankAccount({ id, name, type }: { id: SelectBankAccount["id"], name: string, type: string }) {
-
-
-
-}
--- src/lib/drizzy/queries/bankAccounts/editBankAccount.ts
+++
@@ -1,8 +0,0 @@
-import { eq } from "drizzle-orm";
-import { db } from "../../db";
-import { bankAccounts, SelectBankAccount } from "../../schema/other";
-
-export default async function editBankAccount(id: SelectBankAccount["id"], data: Partial<Omit<SelectBankAccount, "id">>) {
-    const updatedBankAccount = await db.update(bankAccounts).set(data).where(eq(bankAccounts.id, id));
-    return updatedBankAccount;
-}
